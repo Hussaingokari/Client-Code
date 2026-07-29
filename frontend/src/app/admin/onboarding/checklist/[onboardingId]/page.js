d2c35f29f652d0b91b508f943d9567357afb1b57
KafeOvid <ayushmanng04@gmail.com>
2026-07-29 22:14:17 +0530

    Fix static routing on hard refresh by enabling trailingSlash and removing old dynamic route folder

--- frontend/src/app/admin/onboarding/checklist/[onboardingId]/page.js
+++
@@ -1,9 +0,0 @@
-import ChecklistClient from './ChecklistClient';
-
-export async function generateStaticParams() {
-    return [{ onboardingId: 'dummy' }];
-}
-
-export default function Page() {
-    return <ChecklistClient />;
-}
